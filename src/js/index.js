8c2db70641738d3f66c05f3b36066b0fe842a6cc
volkovskiy <volkovskiy.v@gmail.com>
2017-12-01 01:11:10 +0300

    init

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+const a = 4;
+
+function test(value) {
+	return value;
+}
+
+test(a);
+
+test('a');
+
+console.log('tt');
---
+++ webpack.common.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = {
+	plugins: [
+		new CleanWebpackPlugin(['build']),
+		new CopyWebpackPlugin([
+			{
+				from: './img/*',
+				to: '',
+			},
+
+			{
+				from: './*.html',
+				to: '',
+			},
+		]),
+		new ExtractTextPlugin('css/styles.css'),
+		new webpack.optimize.ModuleConcatenationPlugin(),
+	],
+
+	context: path.resolve(__dirname, 'src'),
+	entry: {
+		index: './js/index.js',
+	},
+
+	resolve: {
+		extensions: ['.js'],
+		alias: {
+			$js: path.resolve(__dirname, 'src/js'),
+		},
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: path.resolve(__dirname, 'src/js'),
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+		],
+	},
+
+
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'build'),
+	},
+};
